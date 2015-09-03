28e6f5dbbe00e860edabfcd69af8e9710e26737d
Michelin01jp <michelin1@live.jp>
2015-09-03 16:30:39 +0900

    change method of converting number(s) to string(s)

--- Fizzbuzz/Program.cs
+++ Fizzbuzz/Program.cs
@@ -7,7 +7,7 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            (new int[100]).Select((m, n)=>{n++;Console.WriteLine(n%15==0?"FizzBuzz":(n%3==0?"Fizz":(n%5==0?"Buzz":n.ToString())));return n;}).ToArray();
+            (new int[100]).Select((m, n) => { n++; Console.WriteLine(n % 15 == 0 ? "FizzBuzz" : (n % 3 == 0 ? "Fizz" : (n % 5 == 0 ? "Buzz" : "" + n))); return n; }).ToArray();
         }
     }
 }
